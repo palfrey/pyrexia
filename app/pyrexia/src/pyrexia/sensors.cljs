9603497b62d623b664779337b9271a2386c7c07f
Tom Parker <palfrey@tevp.net>
2015-09-27 20:54:25 +0100

    Fix selection mechanism on sensors

--- app/pyrexia/src/pyrexia/sensors.cljs
+++ app/pyrexia/src/pyrexia/sensors.cljs
@@ -4,22 +4,25 @@
             [reagent.core :as r]))
 
 (defn sensor-view [sensor]
-  ^{:key (first sensor)}
-  [:li #js {:onMouseOver
-            (fn [e]
-              (classes/add (.-target e) "foo")
-              (.stopPropagation e))
-            :onMouseOut
-            (fn [e]
-              (classes/remove (.-target e) "foo")
-              (.stopPropagation e))}
-   (first sensor)
+  (let [id (first sensor)]
+    ^{:key id}
+    [:li {:class "no-box"
+          :id id
+          :onMouseOver
+          (fn [e]
+            (classes/add (.getElementById js/document id) "box")
+            (.stopPropagation e))
+          :onMouseOut
+          (fn [e]
+            (classes/remove (.getElementById js/document id) "box")
+            (.stopPropagation e))}
+     (first sensor)
 
-   ^{:key (str (first sensor) "-ul")}
-   [:ul
-    (map
-     #(with-meta [:li (str (-> % first name) " : " (-> % second))] {:key (-> % first name)})
-     (select-keys (second sensor) [:temp :humid (keyword "@timestamp")]))]])
+     ^{:key (str (first sensor) "-ul")}
+     [:ul
+      (map
+       #(with-meta [:li (str (-> % first name) " : " (-> % second))] {:key (-> % first name)})
+       (select-keys (second sensor) [:temp :humid (keyword "@timestamp")]))]]))
 
 (defn sensors-view []
   [:ul (map sensor-view (into (sorted-map-by <) (:nodes @c/app-state)))])
