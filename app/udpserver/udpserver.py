6035b4fdd5c49360cc2d3af5190ad7284422514f
Tom Parker <palfrey@tevp.net>
2015-11-08 17:12:08 +0000

    Make UDP server dump straight into Elasticsearch and remove old bits

--- app/udpserver/udpserver.py
+++ app/udpserver/udpserver.py
@@ -1,9 +1,24 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 
+from time import strftime, gmtime
+from datetime import datetime
+from elasticsearch import Elasticsearch
+from json import loads
+
+es = Elasticsearch(['elasticsearch'])
+
 class NodeServer(DatagramProtocol):
-    def datagramReceived(self, data, (host, port)):
-        print "received %r from %s:%d" % (data, host, port)
+	def datagramReceived(self, data, (host, port)):
+		print "received %r from %s:%d" % (data, host, port)
+		index = strftime("temperature-%Y.%m.%d", gmtime())
+		doc = loads(data)
+		doc["@timestamp"] = datetime.now()
+		res = es.index(index=index, doc_type='temp', body=doc)
+		if res['created'] == True:
+			print "Sent to Elasticsearch OK"
+		else:
+			print "ERROR! ", res
 
 reactor.listenUDP(3000, NodeServer())
 reactor.run()
