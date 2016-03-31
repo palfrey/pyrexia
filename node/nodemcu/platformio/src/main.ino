3d9bfc5512643f2a254ae0d7750bbb69043c270f
Tom Parker <palfrey@tevp.net>
2016-03-31 14:34:26 +0100

    More bits with testing Arduino stuff

--- node/nodemcu/platformio/src/main.ino
+++ node/nodemcu/platformio/src/main.ino
@@ -4,12 +4,30 @@
  * Turns on an LED on for one second,
  * then off for one second, repeatedly.
  */
-#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+const char* ssid = "******";
+const char* password = "******";
 
 void setup()
 {
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop()
