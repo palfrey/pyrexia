92f097eaba75ad37f0089bac55b7e86d0f2c8885
Tom Parker <palfrey@tevp.net>
2016-04-01 13:03:22 +0100

    Add MQTT send

--- node/nodemcu/platformio/src/main.cpp
+++ node/nodemcu/platformio/src/main.cpp
@@ -4,6 +4,7 @@
 #include "DHT.h"
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
+#include <ArduinoJson.h>
 
 #define DHTPIN 4
 #define DHTTYPE DHT22
@@ -13,12 +14,30 @@ DHT dht(DHTPIN, DHTTYPE);
 
 bool connected = false;
 
+// Store the MQTT server, username, and password in flash memory.
+// This is required for using the Adafruit MQTT library.
+const char MQTT_SERVER[] PROGMEM    = MQTT_HOST;
+const char MQTT_USERNAME[] PROGMEM  = MQTT_USER;
+const char MQTT_PASS[] PROGMEM  = MQTT_PASSWORD;
+
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_PORT, MQTT_USERNAME, MQTT_PASS);
+const char TEMP_FEED[] PROGMEM = "/temp";
+Adafruit_MQTT_Publish temp = Adafruit_MQTT_Publish(&mqtt, TEMP_FEED);
+
+char name[29];
+
 void setup()
 {
 	pinMode(LED_BUILTIN, OUTPUT);
 	Serial.begin(115200);
 	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 	dht.begin();
+
+	byte mac[6];
+	WiFi.macAddress(mac);
+	sprintf(name, "temp-%02x:%02x:%02x:%02x:%02x:%02x", mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
+	Serial.printf("Name: %s\n", name);
 }
 
 void blink(int time) {
@@ -72,6 +91,21 @@ void loop()
 		connected = true;
 	}
 
+	if (!mqtt.connected()) {
+		Serial.println("Connecting to MQTT... " MQTT_HOST " " MQTT_USER);
+		int ret = mqtt.connect();
+		if (ret != 0) {
+			Serial.println(mqtt.connectErrorString(ret));
+			Serial.println("Retrying MQTT connection in 5 seconds...");
+			mqtt.disconnect();
+			delay(5000);
+			return;
+		}
+		else {
+			Serial.println("Connected to " MQTT_HOST " " MQTT_USER);
+		}
+	}
+
 	digitalWrite(LED_BUILTIN, HIGH);
 	// Reading temperature or humidity takes about 250 milliseconds!
 	// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
@@ -81,6 +115,23 @@ void loop()
 	// Read temperature as Fahrenheit (isFahrenheit = true)
 	float f = dht.readTemperature(true);
 	digitalWrite(LED_BUILTIN, LOW);
+
+	StaticJsonBuffer<200> jsonBuffer;
+	JsonObject& root = jsonBuffer.createObject();
+	root["temp"] = t;
+	root["humid"] = h;
+	root["id"] = name;
+
+	char buffer[256];
+	root.printTo(buffer, sizeof(buffer));
+
+	Serial.printf("Buffer: %s\n", buffer);
+
+	if (!temp.publish(buffer)) {
+		Serial.println(F("Failed MQTT"));
+	} else {
+		Serial.println(F("MQTT OK!"));
+	}
 
 	// Check if any reads failed and exit early (to try again).
 	if (isnan(h) || isnan(t) || isnan(f)) {
