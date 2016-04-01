2b9856f8d70dcf4333c3a9dc0b90631c1d74c274
Tom Parker <palfrey@tevp.net>
2016-04-01 14:11:56 +0100

    Remove fahrenheit and heat index bits

--- node/nodemcu/platformio/src/main.cpp
+++ node/nodemcu/platformio/src/main.cpp
@@ -102,7 +102,7 @@ void loop()
 			return;
 		}
 		else {
-			Serial.println("Connected to " MQTT_HOST " " MQTT_USER);
+			Serial.println("Connected");
 		}
 	}
 
@@ -112,8 +112,6 @@ void loop()
 	float h = dht.readHumidity();
 	// Read temperature as Celsius (the default)
 	float t = dht.readTemperature();
-	// Read temperature as Fahrenheit (isFahrenheit = true)
-	float f = dht.readTemperature(true);
 	digitalWrite(LED_BUILTIN, LOW);
 
 	StaticJsonBuffer<200> jsonBuffer;
@@ -139,29 +137,8 @@ void loop()
 		Serial.println(F("MQTT OK!"));
 	}
 
-	// Check if any reads failed and exit early (to try again).
-	if (isnan(h) || isnan(t) || isnan(f)) {
+	if (isnan(h) || isnan(t)) {
 		Serial.println("Failed to read from DHT sensor!");
-	}
-	else {
-		// Compute heat index in Fahrenheit (the default)
-		float hif = dht.computeHeatIndex(f, h);
-		// Compute heat index in Celsius (isFahreheit = false)
-		float hic = dht.computeHeatIndex(t, h, false);
-
-		Serial.print("Humidity: ");
-		Serial.print(h);
-		Serial.print(" %\t");
-		Serial.print("Temperature: ");
-		Serial.print(t);
-		Serial.print(" *C ");
-		Serial.print(f);
-		Serial.print(" *F\t");
-		Serial.print("Heat index: ");
-		Serial.print(hic);
-		Serial.print(" *C ");
-		Serial.print(hif);
-		Serial.println(" *F");
 	}
 	delay(5000);
 }
