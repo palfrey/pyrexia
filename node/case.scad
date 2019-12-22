9b439778a1eba6d3da73edb0b8343bce35d27f5c
Tom Parker-Shemilt <palfrey@tevp.net>
2019-12-22 14:55:36 +0000

    Work towards Platformio 3.x

--- node/case.scad
+++ node/case.scad
@@ -1,16 +1,16 @@
-length = 47;
-width = 31;
-depth = 10;
-thickness = 3;
+length = 48;
+width = 46;
+depth = 21;
+thickness = 2;
 
-usb_width = 5;
-usb_height = 3;
-usb_x = 15;
-usb_y = 5;
+usb_width = 10;
+usb_height = 5;
+usb_x = 16 + thickness;
+usb_y = 1 + thickness;
 
-hole_width = 5;
-hole_length = 10;
-hole_x = 10;
+hole_width = 16;
+hole_length = 8;
+hole_x = 16 + thickness;
 hole_y = (width+2*thickness-hole_length)/2;
 
 difference () {
@@ -21,15 +21,17 @@ difference () {
 		cube([thickness + 1, usb_width, usb_height]);
 }
 
-translate([0,0, depth*2]) {
-	difference() {
-		union() {
-			cube([length + (thickness *2), width + (thickness *2), thickness]);
-			translate([thickness ,thickness, -thickness/2])
-				cube([length, width, thickness/2]);
-		}
+rotate([180,0,0]) {
+	translate([0,10,-thickness]) {
+		difference() {
+			union() {
+				cube([length + (thickness *2), width + (thickness *2), thickness]);
+				translate([thickness ,thickness, -thickness/2])
+					cube([length, width, thickness/2]);
+			}
 
-		translate([hole_x, hole_y, -thickness*1])
-			cube([hole_width, hole_length, thickness*3]);
+			translate([hole_x, hole_y, -thickness*1])
+				cube([hole_width, hole_length, thickness*3]);
+		}
 	}
 }
--- node/nodemcu/platformio/src/main.cpp
+++ node/nodemcu/platformio/src/main.cpp
@@ -1,5 +1,6 @@
-#include <Arduino.h>
+#include "Arduino.h"
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 #include <WiFiClient.h>
 #include "DHT.h"
 #include "Adafruit_MQTT.h"
@@ -9,7 +10,13 @@
 #define DHTPIN 4
 #define DHTTYPE DHT22
 
-#define SECONDS_BETWEEN_READINGS 30
+void ds18b20();
+
+#include <WEMOS_SHT3X.h>
+
+SHT3X sht30(0x45);
+
+#define SECONDS_BETWEEN_READINGS 5
 
 // Setup a DHT22 instance
 DHT dht(DHTPIN, DHTTYPE);
@@ -30,15 +37,34 @@ Adafruit_MQTT_Publish temp = Adafruit_MQTT_Publish(&mqtt, TEMP_FEED);
 
 char name[29];
 
+void onDisconnect(const WiFiEventStationModeDisconnected &event) {
+	Serial.printf("disconnect reason: %d\n", event.reason);
+}
+
+ESP8266WiFiMulti wifiMulti;
+boolean connectionWasAlive = true;
+
 void setup()
 {
+	//ESP.eraseConfig();
 	pinMode(LED_BUILTIN, OUTPUT);
 	Serial.begin(115200);
-	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	Serial.setDebugOutput(true);
+	WiFiEventHandler disconnectedEventHandler = WiFi.onStationModeDisconnected([](const WiFiEventStationModeDisconnected& event)
+	{
+	  Serial.println("Station disconnected");
+  });
+
+
+	//wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+	return;
+	//Serial.printf("Initial status: %d\n", status);
 	wifi_start_time = millis();
-	dht.begin();
+	//dht.begin();
 
 	byte mac[6];
+	WiFi.mode(WIFI_STA);
+	//WiFi.onStationModeDisconnected(onDisconnect);
 	WiFi.macAddress(mac);
 	sprintf(name, "temp-%02x:%02x:%02x:%02x:%02x:%02x", mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
 	Serial.printf("Name: %s\n", name);
@@ -51,51 +77,83 @@ void blink(int time) {
 	delay(time);
 }
 
+void printEncryptionType(int thisType) {
+  // read the encryption type and print out the name:
+  switch (thisType) {
+    case ENC_TYPE_WEP:
+      Serial.println("WEP");
+      break;
+    case ENC_TYPE_TKIP:
+      Serial.println("WPA");
+      break;
+    case ENC_TYPE_CCMP:
+      Serial.println("WPA2");
+      break;
+    case ENC_TYPE_NONE:
+      Serial.println("None");
+      break;
+    case ENC_TYPE_AUTO:
+      Serial.println("Auto");
+      break;
+  }
+}
+
+void listNetworks() {
+  // scan for nearby networks:
+  Serial.println("** Scan Networks **");
+  int numSsid = WiFi.scanNetworks();
+  if (numSsid == -1) {
+    Serial.println("Couldn't get a wifi connection");
+    while (true);
+  }
+
+  // print the list of networks seen:
+  Serial.print("number of available networks:");
+  Serial.println(numSsid);
+
+  // print the network number and name for each network found:
+  for (int thisNet = 0; thisNet < numSsid; thisNet++) {
+    Serial.print(thisNet);
+    Serial.print(") ");
+    Serial.print(WiFi.SSID(thisNet));
+    Serial.print("\tSignal: ");
+    Serial.print(WiFi.RSSI(thisNet));
+    Serial.print(" dBm");
+    Serial.print("\tEncryption: ");
+    printEncryptionType(WiFi.encryptionType(thisNet));
+  }
+}
+
 void retryWifi() {
+	WiFi.disconnect();
+	Serial.printf("Wifi mode: %d\n", WiFi.getMode());
 	blink(1000);
 	connected = false;
-	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	//listNetworks();
+	//blink(1000);
+	wl_status_t status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+	Serial.printf("Initial status: %d\n", status);
 	wifi_start_time = millis();
 }
 
 void loop()
 {
-	wl_status_t status = WiFi.status();
-	switch (status) {
-		case WL_CONNECTED: // all good
-			break;
-		case WL_SCAN_COMPLETED:
-		case WL_IDLE_STATUS:
-		{
-			unsigned long currentMillis = millis();
-			if (currentMillis - wifi_start_time >= 30*1000) {
-				Serial.printf("Been trying to connect for %ld seconds, restarting\n", (currentMillis - wifi_start_time)/1000);
-				retryWifi();
-			}
-			else {
-				Serial.printf("Connecting to wifi (%d)\n", status);
-				blink(500);
-			}
-			return;
-		}
-
-		case WL_NO_SSID_AVAIL:
-			Serial.printf("Problem connecting to wifi: can't find SSID " WIFI_SSID "\n");
-			retryWifi();
-			return;
-
-		case WL_DISCONNECTED:
-		case WL_CONNECT_FAILED:
-		case WL_CONNECTION_LOST:
-			Serial.printf("Problem connecting to wifi: %d\n", status);
-			retryWifi();
-			return;
-
-		case WL_NO_SHIELD:
-			Serial.println("Can't find WiFi Shield!\n");
-			blink(2000);
-			return;
-	}
+	// if (wifiMulti.run() != WL_CONNECTED)
+  //   {
+  //     if (connectionWasAlive == true)
+  //     {
+  //       connectionWasAlive = false;
+  //       Serial.print("Looking for WiFi ");
+  //     }
+  //     Serial.print(".");
+  //     delay(500);
+	//   return;
+  //   }
+  //   else if (connectionWasAlive == false)
+  //   {
+  //     connectionWasAlive = true;
+  //     Serial.printf(" connected to %s\n", WiFi.SSID().c_str());
+  //   }
 
 	if (connected == false) {
 		Serial.print("Connected to ");
@@ -105,7 +163,7 @@ void loop()
 		connected = true;
 	}
 
-	if (!mqtt.connected()) {
+	/*if (!mqtt.connected()) {
 		Serial.println("Connecting to MQTT... " MQTT_HOST " " MQTT_USER);
 		int ret = mqtt.connect();
 		if (ret != 0) {
@@ -118,41 +176,46 @@ void loop()
 		else {
 			Serial.println("Connected");
 		}
-	}
+	}*/
 
 	digitalWrite(LED_BUILTIN, HIGH);
 	// Reading temperature or humidity takes about 250 milliseconds!
 	// Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-	float h = dht.readHumidity();
-	// Read temperature as Celsius (the default)
-	float t = dht.readTemperature();
-	digitalWrite(LED_BUILTIN, LOW);
+	// float h = dht.readHumidity();
+	// // Read temperature as Celsius (the default)
+	// float t = dht.readTemperature();
 
-	StaticJsonBuffer<200> jsonBuffer;
-	JsonObject& root = jsonBuffer.createObject();
-	if (isnan(t))
-		root["temp"] = NULL;
-	else
-		root["temp"] = t;
-	if (isnan(h))
-		root["humid"] = NULL;
-	else
-		root["humid"] = h;
-	root["id"] = name;
+	// sht30.get();
+	// float t = sht30.cTemp;
+	// float h = sht30.humidity;
+	// //digitalWrite(LED_BUILTIN, LOW);
 
-	char buffer[256];
-	root.printTo(buffer, sizeof(buffer));
+	// StaticJsonBuffer<200> jsonBuffer;
+	// JsonObject& root = jsonBuffer.createObject();
+	// if (isnan(t))
+	// 	root["temp"] = NULL;
+	// else
+	// 	root["temp"] = t;
+	// if (isnan(h))
+	// 	root["humid"] = NULL;
+	// else
+	// 	root["humid"] = h;
+	// root["id"] = name;
 
-	Serial.printf("Buffer: %s\n", buffer);
+	// char buffer[256];
+	// root.printTo(buffer, sizeof(buffer));
 
-	if (!temp.publish(buffer)) {
-		Serial.println(F("Failed MQTT"));
-	} else {
-		Serial.println(F("MQTT OK!"));
-	}
+	// Serial.printf("Buffer: %s\n", buffer);
 
-	if (isnan(h) || isnan(t)) {
-		Serial.println("Failed to read from DHT sensor!");
-	}
+	// /*if (!temp.publish(buffer)) {
+	// 	Serial.println(F("Failed MQTT"));
+	// } else {
+	// 	Serial.println(F("MQTT OK!"));
+	// }*/
+
+	// if (isnan(h) || isnan(t)) {
+	// 	Serial.println("Failed to read from DHT sensor!");
+	// }
+	ds18b20();
 	delay(SECONDS_BETWEEN_READINGS * 1000);
 }
